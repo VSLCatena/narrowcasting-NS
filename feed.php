1bcee058f0980ac5e113126311adeb9d787e17a4
adminuser <adminuser@www.klik48.local>
2019-10-24 13:14:08 +0000

    Finalized Buienradar
    Added:
    *Buienradar responsive
    
    Changed:
    *Fixed sizes of all elements
    
    Removed:
    *offset-col-1 from table
    *fixed sizes and font-size
    
    Issues:
    * Maybe some layout issues

--- feed.php
+++ feed.php
@@ -1,10 +1,15 @@
 <?php
 // Set the feed URL.
-$feed_url = 'http://feeds.nos.nl/nosnieuwsalgemeen';
+$url1 = 'http://feeds.nos.nl/nosnieuwsalgemeen';
+$url2 = "http://feeds.nos.nl/nosnieuwsopmerkelijk";
 
 // Fetch the content.
 // See http://php.net/manual/en/function.file-get-contents.php for more
 // information about the file_get_contents() function.
+$feed_url = $url1;
+if($_GET['url']=='2') {
+	$feed_url = $url2;
+	}
 $content = file_get_contents( $feed_url );
 
 // Set the Content-Type header.
