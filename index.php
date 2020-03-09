e1160fd0687381dc4fb4058f4dd80dd0345fed9b
Martijn <Martijn9873@hotmail.com>
2020-03-09 17:26:14 +0100

    Fix bug with file reference

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
 		<div class="row" id="vertrektijdenbase">
 			<div  class="col-12">
 				<div class="row bg-white rcorner" id="title">
-					<div class="col-1"><img src="ns_logo.png" alt="NS Logo" height="24" style="padding:5px"></img></div>
+					<div class="col-1"><img src="NS_logo.png" alt="NS Logo" height="24" style="padding:5px"></img></div>
 					<div class="offset-1 col-2">Vertrektijden</div>
 					<div class="col-sm-8">Leiden Centraal</div>
 				</div>
@@ -99,6 +99,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="http://www.klik48.local/git-narrowcasting-ns/functions.js"></script>
+    <script src="./functions.js"></script>
 	
 </html>
